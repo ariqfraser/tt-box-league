78c7d625e9d575eacc9b6eca92ee01539b0f4a51
ariq <ariqfraser@gmail.com>
2025-11-22 23:06:08 +0000

    Misc: Testing pre-push

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,25 +2,25 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
-  let fixture: ComponentFixture<App>;
-  let component: App;
+    let fixture: ComponentFixture<App>;
+    let component: App;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [App],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(App);
-    component = fixture.componentInstance;
-  });
+        fixture = TestBed.createComponent(App);
+        component = fixture.componentInstance;
+    });
 
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the app', () => {
+        expect(component).toBeFalsy();
+    });
 
-  it('should render title', () => {
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tt-box-league');
-  });
+    it('should render title', () => {
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tt-box-league');
+    });
 });
