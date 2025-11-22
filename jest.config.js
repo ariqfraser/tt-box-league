e1bdf052d219767fe3fa84e4928965e79ad42188
ariq <ariqfraser@gmail.com>
2025-11-22 22:00:07 +0000

    Misc: Configure jest and tsconfig path aliases. Misc: Setup environments

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/main.ts', '!src/index.html'],
-  moduleNameMapper: {
-    '@app/(.*)': '<rootDir>/src/app/$1',
-    '@env/(.*)': '<rootDir>/src/environments/$1',
-  },
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.spec.ts',
+        '!src/main.ts',
+        '!src/index.html',
+        '!src/**/*.{config,routes}.ts',
+    ],
+    moduleNameMapper: {
+        '@app/(.*)': '<rootDir>/src/app/$1',
+        '@env/(.*)': '<rootDir>/src/environments/$1',
+        '@public/(.*)': '<rootDir>/public/$1',
+        '@shared/(.*)': '<rootDir>/src/app/shared/$1',
+        '@core/(.*)': '<rootDir>/src/app/core/$1',
+        '@features/(.*)': '<rootDir>/src/app/features/$1',
+    },
 };
